93f3868c83ae6f97a2b092ae13abeccc41e63900
zakuan1987 <46714346+zakuan1987@users.noreply.github.com>
2019-09-23 13:08:23 -0400

    check_question
    code that checks if user input is a question (ends in a '?' and code keep asking for user input until the user inputs "quit"

--- magic_eight.py
+++ magic_eight.py
@@ -2,3 +2,25 @@ def Homework_3():
     print("What is your question")
 
 Homework_3()
+
+def check_input(response):
+    correct_response = False
+    while correct_response == False:
+        # print(response)
+        if response == "quit":
+            exit()
+        if '?' not in response:
+            print('Every questions ends with a question mark!')
+            response = input(name + ", what is your question for the Magic 8 Ball?")
+        else:
+            correct_response = True
+            return response
+
+name = input("Enter your name:")
+
+# add loop
+while True:
+    response = input(name + ", what is your question for the Magic 8 Ball?")
+    if response == "quit":
+        break
+    response = check_input(response)
